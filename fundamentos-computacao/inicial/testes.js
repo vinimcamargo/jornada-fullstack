0f46e1c954118b1aef7981f2e2d27b208fdba0b2
vinimcamargo <vinicius.machado.camargo@gmail.com>
2025-06-30 14:26:54 -0300

    primeiro commit no git

---
+++ fundamentos-computacao/inicial/testes.js
@@ -0,0 +1,6 @@
+console.log("Olá terráqueos, vim de fora");
+console.log(1+1);
+
+let saida = "tchau";
+
+console.log(saida);
---
+++ fundamentos-computacao/inicial/tipagem.js
@@ -0,0 +1,7 @@
+const pi = 3.1415;
+var nomeVar = "nome 1 var";
+let nomeLet = "nome 1 let";
+
+console.log(pi);
+console.log(nomeVar);
+console.log(nomeLet);
